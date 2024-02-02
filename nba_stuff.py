f62137bc5f340feb49679022ddd16ac09b60f412
Bruno <bmfuh1@umbc.edu>
2024-02-02 00:11:38 -0500

    Adding some helper functions to quickly reference things like player id, team id, etc...

--- nba_stuff.py
+++ nba_stuff.py
@@ -2,15 +2,17 @@ from nba_api.stats.endpoints import playercareerstats
 import typing
 import nba_api.stats.static.players as player
 import nba_api.stats.static.teams as team
-from nba_api.stats.endpoints import playervsplayer
-from nba_api.stats.library.parameters import SeasonAll
+from nba_api.stats.endpoints.boxscorematchupsv3 import BoxScoreMatchupsV3
+# import nba_api.stats.endpoints.boxscorematchupsv3 as boxmatchup
+from nba_api.stats.endpoints import playervsplayer, matchupsrollup
+from nba_api.stats.library.parameters import SeasonAll, Season, SeasonType, PerModeSimple
 from nba_api.stats.endpoints import playergamelogs
 from nba_api.stats.endpoints import playercompare
 import pandas as pd
 
 # career = playercareerstats.PlayerCareerStats()
 m = player.find_players_by_full_name("lebron james")[0]
-k = player.find_players_by_full_name("kevin durant")[0]
+k = player.find_players_by_full_name("Jayson Tatum")[0]
 z = player.get_players()
 l = team.find_teams_by_full_name("phoenix suns")[0]
 p1_id = m['id']
@@ -19,19 +21,27 @@ team_id = l['id']
 # print(m)
 # print(z[0])
 
-print(p1_id)
-print(p2_id)
+# f = matchupsrollup.MatchupsRollup(season=Season.default, per_mode_simple=PerModeSimple.totals, season_type_playoffs=SeasonType.regular, off_player_id_nullable=p1_id, def_player_id_nullable=p2_id)
+# f2 = matchupsrollup.MatchupsRollup(season=Season.default, per_mode_simple=PerModeSimple.totals, season_type_playoffs=SeasonType.regular, off_player_id_nullable=p2_id, def_player_id_nullable=p1_id)
+
+
+# print(f.get_data_frames())
+
+# print(p1_id)
+# print(p2_id)
 
 t = playervsplayer.PlayerVsPlayer(p1_id, p2_id)
-# p1_games = playergamelogs.PlayerGameLogs(player_id_nullable=p1_id,date_from_nullable=None, date_to_nullable=None).player_game_logs.data
+p1_games = playergamelogs.PlayerGameLogs(player_id_nullable=p2_id, season_nullable="2020-21").player_game_logs.data
 # p2_games = playergamelogs.PlayerGameLogs(player_id_nullable=p2_id,date_from_nullable="2020-05-20", date_to_nullable="2022-06-30").player_game_logs.data
 # g = playercompare.PlayerCompare(p1_id,p2_id).data
 # print(g)
 # df1 = pd.DataFrame(data=p1_games['data'], columns=p1_games['headers'])
 # df2 = pd.DataFrame(data=p2_games['data'], columns=p2_games['headers'])
 # df2 = pd.DataFrame(p2_games)
+# h = BoxScoreMatchupsV3('0022001063')
+print()
 # print(p1_games)
 # print(p2_games)
-print(t.headers)
+# print(t.headers)
 # print(df1)
 # print(df2)
---
+++ search_functions.py
@@ -0,0 +1,105 @@
+import pandas
+from nba_api.stats.endpoints import playervsplayer, matchupsrollup, playergamelog
+from nba_api.stats.endpoints import playercareerstats
+import typing
+import nba_api.stats.static.players as player
+import nba_api.stats.static.teams as team
+from nba_api.stats.endpoints.boxscorematchupsv3 import BoxScoreMatchupsV3
+# import nba_api.stats.endpoints.boxscorematchupsv3 as boxmatchup
+from nba_api.stats.endpoints import playervsplayer, matchupsrollup, teamvsplayer
+from nba_api.stats.library.parameters import SeasonAll, Season, SeasonType, PerModeSimple
+from nba_api.stats.endpoints import playergamelogs
+from nba_api.stats.endpoints import playercompare
+import pandas as pd
+
+
+def get_player_id(player_name):
+    player_id = None
+    try:
+        player_id = player.find_players_by_full_name(player_name)[0]['id']
+    except:
+        print("Couldn't find that player's ID")
+    return player_id
+
+
+def get_team_id(team_name):
+    team_id = None
+
+    try:
+        team_id = team.find_teams_by_full_name(team_name)[0]['id']
+    except:
+        print("Couldn't find that team's ID")
+    return team_id
+
+def get_team_name_from_name(team_name):
+    full_team_name = None
+
+    try:
+        full_team_name = team.find_teams_by_full_name(team_name)[0][team_name]
+    except:
+        print("Couldn't find that team's full name")
+    return full_team_name
+def get_team_name_from_id(team_id):
+    team_name = None
+
+    try:
+        team_name = team.find_team_name_by_id(team_id)[0]["full_name"]
+    except:
+        print("Couldn't find that team's name")
+    return team_name
+
+def get_player_vs_player_game_ids(p1_id, p2_id, years: list, season=None):
+    """
+
+    :param years:
+    :param p1_id: player 1's id
+    :param p2_id: player 2's id
+    :return: all the game ids that these two played against each other
+    """
+    p1_gamelog = pandas.concat(playergamelog.PlayerGameLog(player_id=p1_id, season=SeasonAll.all).get_data_frames())
+    p2_gamelog = pandas.concat(playergamelog.PlayerGameLog(player_id=p2_id, season=SeasonAll.all).get_data_frames())
+    players_gamelog = pandas.concat([p1_gamelog, p2_gamelog],
+                                    axis=0)
+
+    # TODO: query the players game log and return the games that they faced each other
+    players_gamelog["GAME_DATE"] = pandas.to_datetime(players_gamelog["GAME_DATE"], format="%b %d, %Y")
+    players_gamelog = players_gamelog.query(f"GAME_DATE.dt.year in {years}")
+    new_log = players_gamelog.sort_values("Game_ID", inplace=False)
+
+    # To find the games that they played each other in, use duplicated to identify which games they were both in.
+    matchups = new_log[new_log["Game_ID"].duplicated(keep=False)]
+    game_ids = matchups["Game_ID"].drop_duplicates()
+
+    # TODO: Maybe I should return the game ids as a list or set
+    return list(game_ids)
+
+
+def get_player_vs_team_game_ids(p_id, team_name, season=None):
+    gamelog = pandas.concat(playergamelog.PlayerGameLog(player_id=p_id,season=SeasonAll.all).get_data_frames())
+
+
+    # gamelog = pandas.concat(teamvsplayer.TeamVsPlayer(vs_player_id=p_id, team_id=team_id).get_data_frames())
+    print("done")
+
+
+def player_vs_player(p1_id, p2_id):
+    """0
+
+    :param p1_id: player 1's id
+    :param p2_id: player 2's id
+    :return: dataset of the time's that player one has played against player two
+    """
+    pass
+
+
+def main():
+    p1_id = get_player_id("Lebron James")
+    p2_id = get_player_id("Kevin Durant")
+    team_id = get_team_id("Lakers")
+    # team_name = Los_Anz
+    # test = get_player_vs_player_game_ids(p1_id, p2_id, [2020,2021,2022,2023])
+    test2 = get_player_vs_team_game_ids(p1_id,"Los Angelos Lakers")
+    print(test2)
+
+
+main()
